9ed69cd65cd2390fc266534e215266755d05525a
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2017-10-06 05:06:11 +0200

    remove comments

--- ElectronNET.WebApp/Program.cs
+++ ElectronNET.WebApp/Program.cs
@@ -1,8 +1,6 @@
 ﻿using ElectronNET.API;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using System;
-using System.Diagnostics;
 
 namespace ElectronNET.WebApp
 {
@@ -13,40 +11,12 @@ namespace ElectronNET.WebApp
             BuildWebHost(args).Run();
         }
 
-        // WICHTIG! UseContentRoot auf Assembly Ordner Essentiell!
-        // Ggf. kann man via Parameter den Content Root durchreichen?
         public static IWebHost BuildWebHost(string[] args)
         {
-            // ToDo: Maybe add a "electronized" args check here?
-            // this is the electron case!
-            //if (args.Length > 0)
-            //{
-                //Console.WriteLine("Test Switch for Electron detection: " + args[0]);
-                return WebHost.CreateDefaultBuilder(args)
-                    .UseElectron(args)
-                    .UseStartup<Startup>()
-                    .Build();
-            //}
-            //else
-            //{
-            //    return WebHost.CreateDefaultBuilder(args)
-            //        .UseStartup<Startup>()
-            //        .Build();
-            //}
-                
-            // this didn't work... its too late, idk... 
-            //var builder = WebHost.CreateDefaultBuilder(args);
-
-            //if (args.Length > 0)
-            //{
-            //    // ToDo: Maybe add a "electronized" args check here?
-            //    Console.WriteLine("Test Switch for Electron detection: " + args[0]);
-            //    builder = builder.UseContentRoot(AppDomain.CurrentDomain.BaseDirectory);
-            //}
-
-            //builder = builder.UseStartup<Startup>();
-
-            //return builder.Build();
+            return WebHost.CreateDefaultBuilder(args)
+                .UseElectron(args)
+                .UseStartup<Startup>()
+                .Build();
         }
     }
 }
