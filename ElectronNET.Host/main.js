cd73d1464e5f91ca13a8a371160cc482b683ef8c
Robert Muehsig <robert.muehsig@codeinside.eu>
2017-10-11 23:18:55 +0200

    osx fix

--- ElectronNET.Host/main.js
+++ ElectronNET.Host/main.js
@@ -70,10 +70,10 @@ app.on('window-all-closed', () => {
     }
 });
 
-app.on('activate', () => {
+//app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow();
-    }
-});
+//    if (win === null) {
+//        createWindow();
+//    }
+//});
