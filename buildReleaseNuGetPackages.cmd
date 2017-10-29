d6ef5e8e100d246db28de102a3a715b54ff9d383
Robert Muehsig <robert.muehsig@codeinside.eu>
2017-10-29 23:11:47 +0100

    https://github.com/ElectronNET/Electron.NET/issues/20

--- buildReleaseNuGetPackages.cmd
+++ buildReleaseNuGetPackages.cmd
@@ -2,7 +2,11 @@ echo "Start building Electron.NET dev stack..."
 echo "Restore & Build API"
 cd ElectronNet.API
 dotnet restore
+dotnet build --configuration Release --force /property:Version=0.0.4
+dotnet pack /p:Version=0.0.4 --configuration Release --force --output "%~dp0artifacts"
 cd ..
 echo "Restore & Build API"
 cd ElectronNet.CLI
 dotnet restore
+dotnet build --configuration Release --force /property:Version=0.0.4
+dotnet pack /p:Version=0.0.4 --configuration Release --force --output "%~dp0artifacts"
