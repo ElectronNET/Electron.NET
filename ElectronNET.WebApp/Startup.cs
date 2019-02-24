a0217c92fa18e7e5e8a0836a7259617fd4c81674
Erik Putrycz <erik@apption.com>
2019-02-24 15:12:42 -0500

    initial update for .net core 3 preview2

--- ElectronNET.WebApp/Startup.cs
+++ ElectronNET.WebApp/Startup.cs
@@ -27,8 +27,8 @@ namespace ElectronNET.WebApp
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
+        {                       
+                //.AddConsole();
 
             if (env.IsDevelopment())
             {
