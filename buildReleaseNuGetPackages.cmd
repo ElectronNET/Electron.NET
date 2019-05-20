aa09af8ad64694eb2e30e84f46e6fb602b2ac182
Robert Muehsig <robert.muehsig@codeinside.eu>
2019-05-20 22:47:21 +0200

    version bump

--- buildReleaseNuGetPackages.cmd
+++ buildReleaseNuGetPackages.cmd
@@ -2,12 +2,12 @@ echo "Start building Electron.NET dev stack..."
 echo "Restore & Build API"
 cd ElectronNet.API
 dotnet restore
-dotnet build --configuration Release --force /property:Version=0.0.11
-dotnet pack /p:Version=0.0.11 --configuration Release --force --output "%~dp0artifacts"
+dotnet build --configuration Release --force /property:Version=5.22.12
+dotnet pack /p:Version=5.22.12 --configuration Release --force --output "%~dp0artifacts"
 cd ..
 echo "Restore & Build CLI"
 cd ElectronNet.CLI
 dotnet restore
-dotnet build --configuration Release --force /property:Version=0.0.11
-dotnet pack /p:Version=0.0.11 --configuration Release --force --output "%~dp0artifacts"
+dotnet build --configuration Release --force /property:Version=5.22.12
+dotnet pack /p:Version=5.22.12 --configuration Release --force --output "%~dp0artifacts"
 cd ..
