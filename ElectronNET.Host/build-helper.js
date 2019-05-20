6d076fb99a21f919ad23816bd3ec1a298717cd03
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2019-05-20 18:38:57 +0200

    update version from Electron.Host/package.json by electron.manifest.json

--- ElectronNET.Host/build-helper.js
+++ ElectronNET.Host/build-helper.js
@@ -1,9 +1,35 @@
 // @ts-ignore
 const manifestFile = require('./bin/electron.manifest');
-const electronBuilderConfig = JSON.stringify({ ...manifestFile.build });
-
 const fs = require('fs');
-fs.writeFile('./bin/electron-builder.json', electronBuilderConfig, (error) => {
+
+const builderConfiguration = { ...manifestFile.build };
+if(builderConfiguration.hasOwnProperty('buildVersion')) {
+    // @ts-ignore
+    const packageJson = require('./package');
+    packageJson.version = builderConfiguration.buildVersion;
+    
+    fs.writeFile('./package.json', JSON.stringify(packageJson), (error) => {
+        if(error) {
+            console.log(error.message);
+        }
+    });
+    
+    try {
+        // @ts-ignore
+        const packageLockJson = require('./package-lock');
+        packageLockJson.version = builderConfiguration.buildVersion;
+        fs.writeFile('./package-lock.json', JSON.stringify(packageLockJson), (error) => {
+            if(error) {
+                console.log(error.message);
+            }
+        });
+    } catch (error) {
+        // ignore missing module
+    }
+}
+
+const builderConfigurationString = JSON.stringify(builderConfiguration);
+fs.writeFile('./bin/electron-builder.json', builderConfigurationString, (error) => {
     if(error) {
         console.log(error.message);
     }
