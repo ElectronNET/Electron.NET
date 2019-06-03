0a02b7140b34172140013376eedc4a6307683f12
kthrose <kthrose@netpoetica.com>
2019-06-03 16:54:25 -0400

    Fix filenames in shell script, ElectronNet != ElectionNET on Linux for builds.

--- buildReleaseNuGetPackages.sh
+++ buildReleaseNuGetPackages.sh
@@ -1,13 +1,15 @@
 echo "Start building Electron.NET dev stack..."
+
 echo "Restore & Build API"
-cd ElectronNet.API
-dotnet restore
-dotnet build --configuration Release --force /property:Version=5.22.12
-dotnet pack /p:Version=5.22.12 --configuration Release --force --output "%~dp0artifacts"
-cd ..
+cd ElectronNET.API && \
+dotnet restore && \
+dotnet build --configuration Release --force /property:Version=5.22.12 && \
+dotnet pack /p:Version=5.22.12 --configuration Release --force --output "%~dp0artifacts";
+
+cd ../;
+
 echo "Restore & Build CLI"
-cd ElectronNet.CLI
-dotnet restore
-dotnet build --configuration Release --force /property:Version=5.22.12
-dotnet pack /p:Version=5.22.12 --configuration Release --force --output "%~dp0artifacts"
-cd ..
+cd ElectronNET.CLI && \
+dotnet restore && \
+dotnet build --configuration Release --force /property:Version=5.22.12 && \
+dotnet pack /p:Version=5.22.12 --configuration Release --force --output "%~dp0artifacts";
