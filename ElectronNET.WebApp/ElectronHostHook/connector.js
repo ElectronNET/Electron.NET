96a5189f143d7fd19f6d114000b6c6268735d8b9
Gustavo Lara Molina <gustavo.lara@softtek.com>
2020-05-12 10:11:04 -0500

    Merge - from master

---
+++ ElectronNET.WebApp/ElectronHostHook/connector.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Connector {
+    constructor(socket, 
+    // @ts-ignore
+    app) {
+        this.socket = socket;
+        this.app = app;
+    }
+    on(key, javaScriptCode) {
+        this.socket.on(key, (...args) => {
+            const id = args.pop();
+            try {
+                javaScriptCode(...args, (data) => {
+                    if (data) {
+                        this.socket.emit(`${key}Complete${id}`, data);
+                    }
+                });
+            }
+            catch (error) {
+                this.socket.emit(`${key}Error${id}`, `Host Hook Exception`, error);
+            }
+        });
+    }
+}
+exports.Connector = Connector;
+//# sourceMappingURL=connector.js.map
