d409c3a0f9d794727297f7dbcf1d013c00ad8278
Frederik Vermeersch <Frederik@oscart.be>
2020-05-22 16:38:34 +0200

    Implementation for electronjs cookie handling

---
+++ ElectronNET.API/Entities/CookieChangedCause.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ElectronNET.API.Entities {
+    /// <summary>
+    /// The cause of the change 
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum CookieChangedCause 
+    {
+        /// <summary>
+        ///The cookie was changed directly by a consumer's action.
+        /// </summary>
+        [JsonProperty("explicit")]
+        @explicit,
+
+        /// <summary>
+        /// The cookie was automatically removed due to an insert operation that overwrote it.
+        /// </summary>
+        overwrite,
+
+        /// <summary>
+        ///  The cookie was automatically removed as it expired.
+        /// </summary>
+        expired,
+
+        /// <summary>
+        ///  The cookie was automatically evicted during garbage collection.
+        /// </summary>
+        evicted,
+
+        /// <summary>
+        ///   The cookie was overwritten with an already-expired expiration date.
+        /// </summary>
+        [JsonProperty("expired_overwrite")]
+        expiredOverwrite
+    }
+}
---
+++ ElectronNET.API/Entities/CookieFilter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectronNET.API.Entities 
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CookieFilter 
+    {
+        /// <summary>
+        /// (optional) - Retrieves cookies which are associated with url.Empty implies retrieving cookies of all URLs.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// (optional) - Filters cookies by name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// (optional) - Retrieves cookies whose domains match or are subdomains of domains.
+        /// </summary>
+        public string Domain { get; set; }
+
+        /// <summary>
+        /// (optional) - Retrieves cookies whose path matches path.
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// (optional) - Filters cookies by their Secure property.
+        /// </summary>
+        public bool Secure { get; set; }
+
+        /// <summary>
+        /// (optional) - Filters out session or persistent cookies.
+        /// </summary>
+        public bool Session { get; set; }
+
+    }
+}
