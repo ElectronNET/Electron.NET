bf8c7c8fa1218b8ec4455c7576ce59ae4f2a697c
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2020-07-06 18:37:31 +0200

    Update to Electron 9.0.5

--- ElectronNET.WebApp/ElectronHostHook/excelCreator.js
+++ ElectronNET.WebApp/ElectronHostHook/excelCreator.js
@@ -9,7 +9,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ExcelCreator = void 0;
 const Excel = require("exceljs");
 class ExcelCreator {
     create(path) {
