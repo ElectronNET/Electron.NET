3838fe897187a3a7850da6604bf9ac5556f31724
Martin Schweizer <m.schweizer@dorner.de>
2021-03-12 16:31:29 +0100

    Add PostData to LoadURLOptions to allow http-posts in LoadURL calls
    Enable the possibility to post data to external Url's with the BrowserWindow.LoadURL function. The post data is provided through LoadURLOptions

---
+++ ElectronNET.API/Entities/UploadFile.cs
@@ -0,0 +1,34 @@
+﻿namespace ElectronNET.API.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UploadFile : IPostData
+    {
+        /// <summary>
+        /// The object represents a file.
+        /// </summary>
+        public string Type { get; } = "file";
+
+        /// <summary>
+        /// The path of the file being uploaded.
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// The offset from the beginning of the file being uploaded, in bytes. Defaults to 0.
+        /// </summary>
+        public long Offset { get; set; } = 0;
+
+        /// <summary>
+        /// The length of the file being uploaded, <see cref="Offset"/>. Defaults to 0.
+        /// If set to -1, the whole file will be uploaded.
+        /// </summary>
+        public long Length { get; set; } = 0;
+
+        /// <summary>
+        /// The modification time of the file represented by a double, which is the number of seconds since the UNIX Epoch (Jan 1, 1970)
+        /// </summary>
+        public double ModificationTime { get; set; }
+    }
+}
---
+++ ElectronNET.API/Entities/UploadRawData.cs
@@ -0,0 +1,18 @@
+﻿namespace ElectronNET.API.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UploadRawData : IPostData
+    {
+        /// <summary>
+        /// The data is available as a Buffer, in the rawData field.
+        /// </summary>
+        public string Type { get; } = "rawData";
+        
+        /// <summary>
+        /// The raw bytes of the post data in a Buffer.
+        /// </summary>
+        public byte[] Bytes { get; set; }
+    }
+}
