9e37d4d857d8d2cad10d546bd970476866838c0f
rafael-aero <rafael@rafael.aero>
2021-12-26 11:00:51 +0100

    fix serializtion

--- ElectronNET.Host/api/desktopCapturer.ts
+++ ElectronNET.Host/api/desktopCapturer.ts
@@ -7,7 +7,20 @@ export = (socket: Socket) => {
     electronSocket = socket;
     socket.on('desktop-capturer-get-sources', (options) => {
         desktopCapturer.getSources(options).then(sources => {
-            electronSocket.emit('desktop-capturer-get-sources-result', sources);
+
+            const result = sources.map(src => {
+                return {
+                    appIcon: src.appIcon,
+                    name: src.name,
+                    id: src.id,
+                    display_id: src.display_id,
+                    thumbnail: { 1: src.thumbnail.toPNG().toString('base64') }
+                };
+            });
+
+            electronSocket.emit('desktop-capturer-get-sources-result', result);
         });
     });
-};
+
+
+};;
