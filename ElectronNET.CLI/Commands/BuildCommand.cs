d929102e18b5a5475ad0a726c120a9c9b8bedda7
rafael-aero <rafael@rafael.aero>
2022-04-10 22:37:43 +0200

    update to Electron 18.0.3

--- ElectronNET.CLI/Commands/BuildCommand.cs
+++ ElectronNET.CLI/Commands/BuildCommand.cs
@@ -10,7 +10,7 @@ namespace ElectronNET.CLI.Commands
 {
     public class BuildCommand : ICommand
     {
-        private const string _defaultElectronVersion = "18.0.2";
+        private const string _defaultElectronVersion = "18.0.3";
 
         public const string COMMAND_NAME = "build";
         public const string COMMAND_DESCRIPTION = "Build your Electron Application.";
