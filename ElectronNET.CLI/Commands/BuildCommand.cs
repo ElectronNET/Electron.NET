4f9b46c61652bbc1ac5ce166f0cb766d163635d0
theolivenbaum <rafael@curiosity.ai>
2022-04-21 09:16:05 +0200

    Electron Release 18.1.0

--- ElectronNET.CLI/Commands/BuildCommand.cs
+++ ElectronNET.CLI/Commands/BuildCommand.cs
@@ -10,7 +10,7 @@ namespace ElectronNET.CLI.Commands
 {
     public class BuildCommand : ICommand
     {
-        private const string _defaultElectronVersion = "17.4.1";
+        private const string _defaultElectronVersion = "18.1.0";
 
         public const string COMMAND_NAME = "build";
         public const string COMMAND_DESCRIPTION = "Build your Electron Application.";
