bfe818c453444925210b8b294b79026d5349ca12
GPU-Fried-Eggs <seele.peng@gmail.com>
2023-09-23 20:43:47 +0300

    Upgrade to use Electron 16

--- src/ElectronNET.CLI/Commands/BuildCommand.cs
+++ src/ElectronNET.CLI/Commands/BuildCommand.cs
@@ -198,7 +198,7 @@ namespace ElectronNET.CLI.Commands
                         : $"node build-helper.js {manifestFileName} {version}", tempPath);
 
                 Console.WriteLine($"Package Electron App for Platform {platformInfo.ElectronPackerPlatform}...");
-                ProcessHelper.CmdExecute($"npx electron-builder --config=./bin/electron-builder.json --{platformInfo.ElectronPackerPlatform} --{electronArch} -c.electronVersion=23.2.0 {electronParams}", tempPath);
+                ProcessHelper.CmdExecute($"npx electron-builder --config=./bin/electron-builder.json --{platformInfo.ElectronPackerPlatform} --{electronArch} -c.electronVersion=26.2.0 {electronParams}", tempPath);
 
                 Console.WriteLine("... done");
 
