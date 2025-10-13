ae12eb49ee7ae136e8da69d6d0c34eb49f0a28b6
softworkz <softworkz@hotmail.com>
2025-10-13 14:17:02 +0200

    ElectronNET.WebApp: Adjust startup to use new onAppReady callback

--- src/ElectronNET.WebApp/Startup.cs
+++ src/ElectronNET.WebApp/Startup.cs
@@ -1,6 +1,4 @@
-﻿using ElectronNET.API;
-using ElectronNET.API.Entities;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,79 +37,6 @@ namespace ElectronNET.WebApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            if (HybridSupport.IsElectronActive)
-            {
-                ElectronBootstrap();
-            }
-        }
-
-        public async void ElectronBootstrap()
-        {
-            //AddDevelopmentTests();
-
-            var browserWindow = await Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions
-            {
-                Width = 1152,
-                Height = 940,
-                Show = false
-            });
-
-            await browserWindow.WebContents.Session.ClearCacheAsync();
-
-            browserWindow.OnReadyToShow += () => browserWindow.Show();
-            browserWindow.SetTitle(Configuration["DemoTitleInSettings"]);
-        }
-
-        private static void AddDevelopmentTests()
-        {
-            // NOTE: on mac you will need to allow the app to post notifications when asked.
-
-            Electron.App.On("activate", (obj) =>
-            {
-                // obj should be a boolean that represents where there are active windows or not.
-                var hasWindows = (bool) obj;
-
-                Electron.Notification.Show(
-                    new NotificationOptions("Activate", $"activate event has been captured. Active windows = {hasWindows}")
-                    {
-                        Silent = false,
-                    });
-            });
-
-            Electron.Dock.SetMenu(new[]
-            {
-                new MenuItem
-                {
-                    Type = MenuType.normal,
-                    Label = "MenuItem",
-                    Click = () =>
-                    {
-                        Electron.Notification.Show(new NotificationOptions(
-                            "Dock MenuItem Click",
-                            "A menu item added to the Dock was selected;"));
-                    },
-                },
-                new MenuItem
-                {
-                    Type = MenuType.submenu,
-                    Label = "SubMenu",
-                    Submenu = new[]
-                    {
-                        new MenuItem
-                        {
-                            Type = MenuType.normal,
-                            Label = "Sub MenuItem",
-                            Click = () =>
-                            {
-                                Electron.Notification.Show(new NotificationOptions(
-                                    "Dock Sub MenuItem Click",
-                                    "A menu item added to the Dock was selected;"));
-                            },
-                        },
-                    }
-                }
             });
         }
     }
