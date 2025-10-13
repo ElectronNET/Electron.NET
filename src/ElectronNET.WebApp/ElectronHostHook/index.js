f81424978e31df140999353d1ca77725f0ef2231
softworkz <softworkz@hotmail.com>
2025-10-13 14:13:47 +0200

    ElectronNET.WebApp: HostHook: Make it compile properly

--- src/ElectronNET.WebApp/ElectronHostHook/index.js
+++ src/ElectronNET.WebApp/ElectronHostHook/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.HookService = void 0;
 const connector_1 = require("./connector");
@@ -19,11 +10,11 @@ class HookService extends connector_1.Connector {
     }
     onHostReady() {
         // execute your own JavaScript Host logic here
-        this.on("create-excel-file", (path, done) => __awaiter(this, void 0, void 0, function* () {
+        this.on("create-excel-file", async (path, done) => {
             const excelCreator = new excelCreator_1.ExcelCreator();
-            const result = yield excelCreator.create(path);
+            const result = await excelCreator.create(path);
             done(result);
-        }));
+        });
     }
 }
 exports.HookService = HookService;
