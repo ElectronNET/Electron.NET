e09ef35be46e0f314d3b9bf48ed201e9eb5ce9d1
agracio <agracio@users.noreply.github.com>
2025-12-04 18:08:42 +0000

    removing incorrect import

--- src/ElectronNET.Host/api/webContents.ts
+++ src/ElectronNET.Host/api/webContents.ts
@@ -1,5 +1,5 @@
 import { Socket } from "net";
-import {BrowserWindow, BrowserView, webFrame} from "electron";
+import {BrowserWindow, BrowserView} from "electron";
 import { browserViewMediateService } from "./browserView";
 const fs = require("fs");
 let electronSocket;
