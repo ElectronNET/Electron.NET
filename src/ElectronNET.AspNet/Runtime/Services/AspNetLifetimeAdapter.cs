bb4337a31c12bed129765e561c8c1ac576904d55
Pierre Arnaud <arnaud@epsitec.ch>
2026-01-31 15:37:35 +0100

    Cosmetic

--- src/ElectronNET.AspNet/Runtime/Services/AspNetLifetimeAdapter.cs
+++ src/ElectronNET.AspNet/Runtime/Services/AspNetLifetimeAdapter.cs
@@ -1,27 +1,28 @@
-﻿namespace ElectronNET.AspNet.Runtime
+﻿using ElectronNET.Runtime.Data;
+using ElectronNET.Runtime.Services;
+
+using Microsoft.Extensions.Hosting;
+
+using System.Threading.Tasks;
+
+namespace ElectronNET.AspNet.Runtime;
+
+internal class AspNetLifetimeAdapter : LifetimeServiceBase
 {
-    using System.Threading.Tasks;
-    using ElectronNET.Runtime.Data;
-    using ElectronNET.Runtime.Services;
-    using Microsoft.Extensions.Hosting;
+    private readonly IHostApplicationLifetime lifetimeService;
 
-    internal class AspNetLifetimeAdapter : LifetimeServiceBase
+    public AspNetLifetimeAdapter(IHostApplicationLifetime lifetimeService)
     {
-        private readonly IHostApplicationLifetime lifetimeService;
+        this.lifetimeService = lifetimeService;
 
-        public AspNetLifetimeAdapter(IHostApplicationLifetime lifetimeService)
-        {
-            this.lifetimeService = lifetimeService;
+        this.lifetimeService.ApplicationStarted.Register(() => this.TransitionState(LifetimeState.Ready));
+        this.lifetimeService.ApplicationStopping.Register(() => this.TransitionState(LifetimeState.Stopping));
+        this.lifetimeService.ApplicationStopped.Register(() => this.TransitionState(LifetimeState.Stopped));
+    }
 
-            this.lifetimeService.ApplicationStarted.Register(() => this.TransitionState(LifetimeState.Ready));
-            this.lifetimeService.ApplicationStopping.Register(() => this.TransitionState(LifetimeState.Stopping));
-            this.lifetimeService.ApplicationStopped.Register(() => this.TransitionState(LifetimeState.Stopped));
-        }
-
-        protected override Task StopCore()
-        {
-            this.lifetimeService.StopApplication();
-            return Task.CompletedTask;
-        }
+    protected override Task StopCore()
+    {
+        this.lifetimeService.StopApplication();
+        return Task.CompletedTask;
     }
 }
